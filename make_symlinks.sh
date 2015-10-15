ea836d4a6403782d0c6988aabad3abe6ad96e137
anaro <anaro1017@gmail.com>
2015-10-16 02:42:42 +0900

    linkを作らない対象を記述できる

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -2,7 +2,8 @@
 
 dots_dir=$(cd $(dirname $0); pwd)
 script_name=${0##*/}
-for e in `ls $dots_dir`
+ignores="bash_profile"
+for e in `ls $dots_dir | grep -v $ignores`
 do
   if [[ $script_name != $e ]]; then
     ln -nfs $dots_dir/$e $HOME/.$e
